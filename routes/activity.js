68e76894c0b60df6f23df5de468fdbe19d68f0d1
derek-mcmullen <derek.m.mcmullen@gmail.com>
2018-11-09 20:27:43 +0000

    testing authenticate

--- routes/activity.js
+++ routes/activity.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var router = express.Router();
+var router = express.Router(); 
 
 // Import the model for Activity documents
 var Activity = require("../models/activity"); 
