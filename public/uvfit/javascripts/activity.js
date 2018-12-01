0897546c41b929b58da32bdf6ee7d9719b9b506e
EC2 Default User <ec2-user@ip-172-31-40-31.us-east-2.compute.internal>
2018-12-01 03:10:12 +0000

    Table is fully functional right now

--- public/uvfit/javascripts/activity.js
+++ public/uvfit/javascripts/activity.js
@@ -4,61 +4,18 @@ function sendReqForActivityInfo(deviceId) {
       type: 'GET',
       headers: { 'x-auth': window.localStorage.getItem("authToken") },
       responseType: 'json',
-      success: activityInfoSuccess1,
+      success: activityInfoSuccess,
       error: activityInfoError
    });
 }
 
-function activityInfoSuccess(data, textStatus, jqXHR) {
-   	var prepAllString = "";
-	var prepWalkingString = "";
-	var prepRunningString = "";
-	var prepBikingString = "";
-	var walkHasData = "";
-	var runHasData = "";
-	var bikeHasData = "";
-	for (var activity of data.activity) {
-   		if(activity.activityId === -1) { 
-			prepAllString = "<tr><td valign='top' colspan='6' class='dataTables_empty'>This device currently has no data to show</td></tr>";
-			prepWalkingString = "<tr><td valign='top' colspan='6' class='dataTables_empty'>This device currently has no data to show</td></tr>";
-			prepRunningString = "<tr><td valign='top' colspan='6' class='dataTables_empty'>This device currently has no data to show</td></tr>";
-			prepBikingString = "<tr><td valign='top' colspan='6' class='dataTables_empty'>This device currently has no data to show</td></tr>";
-   		} 
-		else {  
-			prepAllString += "<tr><td>" + activity.activityType + "</td><td>" + "Date Needed" + "</td><td>" + activity.duration + "</td><td>" + "Calories Needed" + "</td><td>" + activity.uv + "</td><td>" + activity.speed + "</td></tr>";
-			if(activity.activityType === "Walking"){
-				prepWalkingString += "<tr><td>" + "Date Needed" + "</td><td>" + activity.duration + "</td><td>" + "Calories Needed" + "</td><td>" + activity.uv + "</td><td>" + activity.speed + "</td></tr>";
-				walkHasData = "data";
-			}
-			if(activity.activityType === "Running"){
-				prepRunningString += "<tr><td>" + "Date Needed" + "</td><td>" + activity.duration + "</td><td>" + "Calories Needed" + "</td><td>" + activity.uv + "</td><td>" + activity.speed + "</td></tr>";
-				runHasData = "data";
-			}
-			if(activity.activityType === "Biking"){
-				prepBikingString += "<tr><td>" + "Date Needed" + "</td><td>" + activity.duration + "</td><td>" + "Calories Needed" + "</td><td>" + activity.uv + "</td><td>" + activity.speed + "</td></tr>";
-				bikeHasData = "data";
-			}
-		}
-		}
-	if (walkHasData !== "data"){
-				prepWalkingString = "<tr><td valign='top' colspan='6' class='dataTables_empty'>This device currently has no data to show</td></tr>";
-			}
-	if(runHasData !== "data"){
-				prepRunningString = "<tr><td valign='top' colspan='6' class='dataTables_empty'>This device currently has no data to show</td></tr>";
-			}			
-	if(bikeHasData !== "data"){
-				prepBikingString = "<tr><td valign='top' colspan='6' class='dataTables_empty'>This device currently has no data to show</td></tr>";
-	}
-	
-   	$("#activityAllTableData").html(prepAllString);
-	$("#activityWalkingTableData").html(prepWalkingString);
-	$("#activityRunningTableData").html(prepRunningString);
-	$("#activityBikingTableData").html(prepBikingString);
-   	$("#error").hide();
-	$("#main").show();
+function activityInfoSuccess(data, textStatus, jqXHR){
+	allTablePopulate(data, textStatus, jqXHR);
+	walkingTablePopulate(data, textStatus, jqXHR);
+	runningTablePopulate(data, textStatus, jqXHR);
+	bikingTablePopulate(data, textStatus, jqXHR);
 }
-
-function activityInfoSuccess1(data, textStatus, jqXHR){
+function allTablePopulate(data, textStatus, jqXHR){
 	var data1 = [];
 	var i = 0;
 	var emptyTable = false;
@@ -67,25 +24,28 @@ function activityInfoSuccess1(data, textStatus, jqXHR){
 			emptyTable = true;
 		}
 		else{
+		var durationString = parseFloat(activityDuration(activity.duration));
 		var tabledata = 
-		{actType: activity.activityType, date: "date", duration: activity.duration, calories: "calories", uv: activity.uv, speed: activity.speed};
+		{actType: activity.activityType, date: "date", duration: durationString, calories: "calories", uv: activity.uv, speed: parseFloat(activity.speed.toFixed(1))};
 		}
 		data1[i] = tabledata;
 		i += 1;
 	}	
 	if(!emptyTable){
-		var table = new Tabulator("#table-here", {
-			height:"45vh",
+		var table = new Tabulator("#all-table-here", {
+			height:"44vh",
+			pagination:"local",
+    		paginationSize:10,
 			data:data1,
 			responsiveLayout:"hide",
 			layout:"fitColumns",
 			columns:[
 					{title:"Activity Type",field:"actType",align:"center"},
 					{title:"Date of Activity",field:"date",align:"center"},
-					{title:"Duration of Activity",field:"duration",align:"center"},
+					{title:"Duration of Activity",field:"duration",align:"center",bottomCalc:"avg"},
 					{title:"Calories Burned",field:"calories",align:"center"},
-					{title:"UV Index",field:"uv",align:"center"},
-					{title:"Speed",field:"speed",align:"center"},
+					{title:"UV Index",field:"uv",align:"center",bottomCalc:"avg"},
+					{title:"Speed",field:"speed",align:"center",bottomCalc:"avg"},
 			],
 			rowClick:function(e, row){ //trigger an alert message when the row is clicked
  				alert("Row " + row.getData().id + " Clicked!!!!");
@@ -101,7 +61,147 @@ function activityInfoSuccess1(data, textStatus, jqXHR){
 		$("#main").show();
 	}
 }
-
+function walkingTablePopulate(data, textStatus, jqXHR){
+	var data1 = [];
+	var i = 0;
+	var emptyTable = false;
+	for (var activity of data.activity) {
+		if(activity.activityId === -1){
+			emptyTable = true;
+		}
+		else{
+		var durationString = parseFloat(activityDuration(activity.duration));
+		var tabledata = 
+		{date: "date", duration: durationString, calories: "calories", uv: activity.uv, speed: parseFloat(activity.speed.toFixed(1))};
+		}
+		if(activity.activityType === "Walking"){
+		data1[i] = tabledata;
+		}
+		i += 1;
+	}	
+	if(!emptyTable){
+		var table = new Tabulator("#walking-table-here", {
+			height:"44vh",
+			pagination:"local",
+    		paginationSize:10,
+			data:data1,
+			responsiveLayout:"hide",
+			layout:"fitColumns",
+			columns:[
+					{title:"Date of Activity",field:"date",align:"center"},
+					{title:"Duration of Activity",field:"duration",align:"center",bottomCalc:"avg"},
+					{title:"Calories Burned",field:"calories",align:"center"},
+					{title:"UV Index",field:"uv",align:"center",bottomCalc:"avg"},
+					{title:"Speed",field:"speed",align:"center",bottomCalc:"avg"},
+			],
+			rowClick:function(e, row){ //trigger an alert message when the row is clicked
+ 				alert("Row " + row.getData().id + " Clicked!!!!");
+ 			},
+		});
+	$("#error").hide();
+	$("#main").show();
+	}
+	else{
+		var prepString = "<h3>There is currently no data for this device to display</h3>";
+		$("#table-here").html(prepString);
+		$("#error").hide();
+		$("#main").show();
+	}
+}
+function runningTablePopulate(data, textStatus, jqXHR){
+	var data1 = [];
+	var i = 0;
+	var emptyTable = false;
+	for (var activity of data.activity) {
+		if(activity.activityId === -1){
+			emptyTable = true;
+		}
+		else{
+		var durationString = parseFloat(activityDuration(activity.duration));
+		var tabledata = 
+		{date: "date", duration: durationString, calories: "calories", uv: activity.uv, speed: parseFloat(activity.speed.toFixed(1))};
+		}
+		if(activity.activityType === "Running"){
+		data1[i] = tabledata;
+		}
+		i += 1;
+	}	
+	if(!emptyTable){
+		var table = new Tabulator("#running-table-here", {
+			height:"44vh",
+			pagination:"local",
+    		paginationSize:10,
+			data:data1,
+			responsiveLayout:"hide",
+			layout:"fitColumns",
+			columns:[
+					{title:"Date of Activity",field:"date",align:"center"},
+					{title:"Duration of Activity",field:"duration",align:"center",bottomCalc:"avg"},
+					{title:"Calories Burned",field:"calories",align:"center"},
+					{title:"UV Index",field:"uv",align:"center",bottomCalc:"avg"},
+					{title:"Speed",field:"speed",align:"center",bottomCalc:"avg"},
+			],
+			rowClick:function(e, row){ //trigger an alert message when the row is clicked
+ 				alert("Row " + row.getData().id + " Clicked!!!!");
+ 			},
+		});
+	$("#error").hide();
+	$("#main").show();
+	}
+	else{
+		var prepString = "<h3>There is currently no data for this device to display</h3>";
+		$("#table-here").html(prepString);
+		$("#error").hide();
+		$("#main").show();
+	}
+}
+function bikingTablePopulate(data, textStatus, jqXHR){
+	var data1 = [];
+	var i = 0;
+	var emptyTable = false;
+	for (var activity of data.activity) {
+		if(activity.activityId === -1){
+			emptyTable = true;
+		}
+		else{
+		var durationString = parseFloat(activityDuration(activity.duration));
+		var tabledata = 
+		{date: "date", duration: durationString, calories: "calories", uv: activity.uv, speed: parseFloat(activity.speed.toFixed(1))};
+		}
+		if(activity.activityType === "Biking"){
+		data1[i] = tabledata;
+		}
+		i += 1;
+	}	
+	if(!emptyTable){
+		var table = new Tabulator("#biking-table-here", {
+			height:"44vh",
+			pagination:"local",
+    		paginationSize:10,
+			data:data1,
+			responsiveLayout:"hide",
+			layout:"fitColumns",
+			columns:[
+					{title:"Date of Activity",field:"date",align:"center"},
+					{title:"Duration of Activity",field:"duration",align:"center",bottomCalc:"avg"},
+					{title:"Calories Burned",field:"calories",align:"center"},
+					{title:"UV Index",field:"uv",align:"center",bottomCalc:"avg"},
+					{title:"Speed",field:"speed",align:"center",bottomCalc:"avg"},
+			],
+			rowClick:function(e, row){ //trigger an alert message when the row is clicked
+ 				alert("Row " + row.getData().id + " Clicked!!!!");
+ 			},
+		});
+	$("#error").hide();
+	$("#main").show();
+	}
+	else{
+		var prepString = "<h3>There is currently no data for this device to display</h3>";
+		$("#table-here").html(prepString);
+		$("#error").hide();
+		$("#main").show();
+	}
+}
 function activityInfoError(jqXHR, textStatus, errorThrown) {
    // If authentication error, delete the authToken 
    // redirect user to sign-in page (which is index.html)
@@ -114,6 +214,10 @@ function activityInfoError(jqXHR, textStatus, errorThrown) {
      $("#error").html("Error: " + status.message);
      $("#error").show();
    } 
+}
+function activityDuration(length){
+	var duration = (length/60).toFixed(1);
+	return(duration);
 }
 function updateRadio(){
 	var deviceNum = $("input[name='device']:checked").val();
