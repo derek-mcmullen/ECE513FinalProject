4e7d9d275c79aeab96c8ef9124efacb951ea8816
EC2 Default User <ec2-user@ip-172-31-40-31.us-east-2.compute.internal>
2018-12-07 18:06:56 +0000

    community actvitiy

--- public/uvfit/javascripts/activityCommunity.js
+++ public/uvfit/javascripts/activityCommunity.js
@@ -1,51 +1,73 @@
-/*function geocodeAddress(geocoder) {
-	var geocoder = new google.maps.Geocoder();
-	console.log("button Pressed");
-	var address = 85737;
-	geocoder.geocode({'address': address}, function(results, status) {
-          if (status === 'OK') {
-            var geocodedCoord = results[0].geometry.location;
-			//console.log(String(geocodedCoord));
-          } else {
-            alert('Geocode was not successful for the following reason: ' + status);
-          }
-        });
-	
-});
-});*/
-var mapData2 = {
-    	lat: 33,
-    	lng: -112
-	};
+function getCoordinates(zipcode){
 $.ajax({
   url: 'https://geocoder.api.here.com/6.2/geocode.json',
   type: 'GET',
   dataType: 'jsonp',
   jsonp: 'jsoncallback',
   data: {
-    searchtext: '85742 USA',
+    searchtext: zipcode + ' USA',
     app_id: 'nZc4mz1eOhPWxpoaexPX',
     app_code: 'ZQYM-bRmTUtvs85ho37uug',
     gen: '9'
   },
   success: function (data) {
     var location = data.Response.View[0].Result;
-  	var mapData = {
-    	lat: location[0].Location.DisplayPosition.Latitude,
-    	lng: location[0].Location.DisplayPosition.Longitude
-	};
-	console.log(mapData);
-	var answer = arePointsNear(mapData, mapData2, 150);
-	console.log(answer);
+    var lat = location[0].Location.DisplayPosition.Latitude;
+    var lng = location[0].Location.DisplayPosition.Longitude;
+	postCoordinates(lat,lng);
+	
   }
 });
-function arePointsNear(checkPoint, centerPoint, km) {
-    var ky = 40000 / 360;
-    var kx = Math.cos(Math.PI * centerPoint.lat / 180.0) * ky;
-    var dx = Math.abs(centerPoint.lng - checkPoint.lng) * kx;
-    var dy = Math.abs(centerPoint.lat - checkPoint.lat) * ky;
-    return Math.sqrt(dx * dx + dy * dy) <= km;
 }
-//$(function(){
-//$("#geocodeButton").click(fuction(){});
-//});
+
+function postCoordinates(lat,lng){
+	$.ajax({
+        url: '/activity/local',
+        type: 'POST',
+        headers: { 'x-auth': window.localStorage.getItem("authToken") },   
+        data: { centerLat: lat, centerLng: lng, miles: $("#mileRadius").val()}, 
+        responseType: 'json',
+        success: function (data, textStatus, jqXHR) {
+           // Add new device to the device list
+           $("#zipcode").val("");
+			$("#localUserData").show();
+		   $("#error").hide();
+			
+			//
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            var response = JSON.parse(jqXHR.responseText);
+            $("#error").html("Error: " + response.message);
+            $("#error").show();
+        }
+    }); 
+}
+function buttonPress(){
+	var enterZipcode = $("#zipcode").val();
+	getCoordinates(enterZipcode);
+}
+function getAllUserData(){
+	$.ajax({
+      url: '/activity/location/' + actId,
+      type: 'GET',
+      headers: { 'x-auth': window.localStorage.getItem("authToken") },
+      responseType: 'json',
+      success: function (data, textStatus, jqXHR) {
+           // Add new device to the device list
+           $("#activityCountAllAll").html(data.allActCount); 
+		   $("#error").hide();
+			
+			//
+        },
+      error: function(jqXHR, textStatus, errorThrown) {
+            var response = JSON.parse(jqXHR.responseText);
+            $("#error").html("Error: " + response.message);
+            $("#error").show();
+        }
+   });
+}
+//On page load
+$(function(){
+	$("#localUserData").hide();
+	$("#submitButton").click(buttonPress);
+});
